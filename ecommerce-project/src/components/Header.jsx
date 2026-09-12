1ee9666f13a82ba37d2cd91e54742ed9d2ec1610
Aniket Ribadiya <aniketribadiya@gmail.com>
2026-09-12 10:10:47 +0530

    Add server proxy config fro /api and /images

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 export function Header( { cart }) {
     let totalQuantity = 0;
 
-    cart.forEach(cartItem => {
+    cart.forEach((cartItem) => {
         totalQuantity += cartItem.quantity
     })
     return (
